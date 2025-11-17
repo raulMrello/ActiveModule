c9caf328236b76cb132a3cf5f6320cb488fc8c6d
luisjerm <luisjeronimomoya@gmail.com>
2025-11-17 13:16:22 +0100

    bugfix return

--- GlobalActiveModule.h
+++ GlobalActiveModule.h
@@ -10,7 +10,7 @@ class GlobalActiveModule{
       }
       virtual ~GlobalActiveModule() = default;
       bool getLogActive() { return _logActive; }
-      char* getLogName() { return _logName; }
+      char* getLogName() { return (char*)_logName; }
       void setJSONSupport(bool flag){_json_supported = flag;}
       bool isJSONSupported(){return _json_supported;}
       virtual osStatus putMessage(State::Msg *msg) = 0;
